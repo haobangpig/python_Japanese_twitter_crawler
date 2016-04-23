c58dc036ad34f0232c33e60093e4ec76c1f7a6a7
haobangpig <haobangpig@gmail.com>
2016-04-23 16:20:36 +0900

    need repair again

--- twitter.py
+++ twitter.py
@@ -3,6 +3,7 @@ import tweepy
 import MySQLdb
 import json
 import re
+import time 
 consumer_key = "Y3QHmimWrVJKySnjnAA1eWZTf"
 consumer_secret = "m8MRiZFUSi50M4dYVJIzWvnUGeLSfn6FqgEKSr4bipCa6NvlXN"
 access_key = "717953299958247424-Au3CpITFDW7CpYzIZghzhfc34pQuGvz"
@@ -13,7 +14,8 @@ host = 'localhost' # your host
 db = 'test' # database where your table is stored
 table = 'twitter' # table you want to save
 table2 = 'user'
-
+con = MySQLdb.connect(user=users, passwd=passwd, host=host, db=db,use_unicode=0,charset='utf8')
+c = con.cursor()
 def get_all_tweets(screen_name):
 	#initialize a list to hold all the tweepy Tweets
 	alltweets = []		
@@ -34,7 +36,7 @@ def get_all_tweets(screen_name):
 		oldest = alltweets[-1].id - 1		
 		print "...%s tweets downloaded so far" % (len(alltweets))
 	return alltweets
-	
+
 
 if __name__ == '__main__':
 	#authorize twitter, initialize tweepy
@@ -42,39 +44,21 @@ if __name__ == '__main__':
 	auth.set_access_token(access_key, access_secret)
 	api = tweepy.API(auth)
 	#pass in the username of the account you want to download
-	con = MySQLdb.connect(user=users, passwd=passwd, host=host, db=db,use_unicode=0,charset='utf8')
-	cursor = con.cursor()
+	
 	query = "SELECT screen_name FROM %s;" % table2
-	cursor.execute(query)
-	result=cursor.fetchall()
-	count =0;
+	c.execute(query)
+	result=c.fetchall()
+	count =0
 	#for users in result:
 	for users in result:
-		alltweets=get_all_tweets("haobangpig")
-		cursor = con.cursor()
-		string = ''.join(alltweets)
-		print string
-		'''
-	        if 'Status' in tweet:
-	            user = tweet['user']
-	            time= tweet['created_at']
-	            ids = tweet['id']
-	            language=tweet['lang']
-	            screen_name=user['screen_name'].encode('utf-8')
-	            username=user['name'].encode('utf-8')
-	            location=user['location']
-	            text = re.sub(r'\n',r' ',tweet['text'].encode('utf-8')) # 改行コード除去
-	            print tweet['created_at'],":\t",tweet['id'],":\t",tweet['lang'],":\t",user['screen_name'].encode('utf-8'),":\t", \
-	                          user['name'].encode('utf-8'),":\t Location：", user['location'],":",text        
-	            con.execute("INSERT INTO twitter_ja (time, id, language,screen_name,username,location,text) VALUES (%s,%s,%s,%s,%s,%s,%s)",(time,ids,language,screen_name,username,location,text))
-
-
-
-
-
-
-		for tweet in alltweets:
-			cursor.execute("INSERT INTO twitter (time,id,language,screen_name,location) VALUES (alltweets['created_at'],alltweets['id_str'],alltweets['lang'],user['screen_name'],user['location'],%s"),(alltweets['text'].encode('utf-8'))
-		cursor.close()
+		all = get_all_tweets("haobangpig")
+		for tweet in all:			
+			time=  [tweet.created_at]
+			str_time = time.date().isoformat()
+			id = [tweet.id_str]
+			language= [tweet.lang]
+			text = [tweet.text.encode("utf-8")]
+			#c.execute("INSERT INTO twitter (time, language,id,location,text) VALUES (%s,%s,%s,%s)",(time,language,id,text))
+			print str_time,id,language,text 
+		c.close()
 		con.commit()
-		'''
